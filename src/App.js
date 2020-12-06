f2c4d873de3d6141774e708f03d379029d463919
eralmendral <eralmendral@gmail.com>
2020-12-07 03:15:23 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+export default class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className='container'>
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <div>
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={icon}></i> {title}
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Search",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
